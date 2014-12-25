d9d1ccbc82eff5ae7a9e6ff295b5422c4599e73d
Roman Kříž <samnung@gmail.com>
2014-12-25 23:01:04 +0100

    [OPFGenerator] refactoring + creating spine

--- lib/Epuber/main_controller/opf_generator.rb
+++ lib/Epuber/main_controller/opf_generator.rb
@@ -3,6 +3,8 @@ require 'time'
 require 'nokogiri'
 require 'mime-types'
 
+require_relative '../book/toc_item'
+
 
 module Epuber
   class MainController
@@ -39,58 +41,16 @@ module Epuber
       # @return [Nokogiri::XML::Document]
       #
       def generate_opf
-        epub_version = @target.epub_version
-
         builder = Nokogiri::XML::Builder.new(encoding: 'utf-8') { |xml|
+          @xml = xml
 
-          xml.package(package_namespaces, :version => epub_version, 'unique-identifier' => OPF_UNIQUE_ID) {
-            xml.metadata {
-              if @book.title
-                if epub_version >= 3
-                  xml['dc'].title(@book.title, id: 'title')
-                  xml.meta('main', refines: '#title', property: 'title-type')
-                else
-                  xml['dc'].title(@book.title)
-                end
-              end
-
-              authors = @book.authors || []
-              authors.each_index { |idx|
-                author        = authors[idx]
-                author_id     = "author_#{idx + 1}"
-                author_id_ref = "##{author_id}"
-
-                if epub_version >= 3
-                  xml['dc'].creator(author.pretty_name, id: author_id)
-                  xml.meta(author.file_as, property: 'file-as', refines: author_id_ref)
-                  xml.meta(author.role, property: 'role', refines: author_id_ref, scheme: 'marc:relators')
-                else
-                  xml['dc'].creator(author.pretty_name)
-                end
-              }
-
-              xml['dc'].publisher(@book.publisher) unless @book.publisher.nil?
-              xml['dc'].language(@book.language) unless @book.language.nil?
-              xml['dc'].date(@book.published.iso8601) unless @book.published.nil?
-
-              xml.meta(name: 'cover', content: create_id_from_path(@target.cover_image.destination_path)) unless @target.cover_image.nil?
-
-              if epub_version >= 3
-                xml.meta(Time.now.utc.iso8601, property: 'dcterms:modified')
-
-                if @target.is_ibooks
-                  xml.meta(@book.version, property: 'ibooks:version') unless @book.version.nil?
-                  xml.meta(@book.custom_fonts, property: 'ibooks:specified-fonts') unless @book.custom_fonts.nil?
-                end
-              end
-            }
-            xml.manifest {
-              @target.all_files.each { |file|
-                xml.item(id: create_id_from_path(file.destination_path), href: file.destination_path, 'media-type' => mime_type_for(file))
-              }
-            }
-            xml.spine
+          xml.package(package_namespaces, :version => @target.epub_version, 'unique-identifier' => OPF_UNIQUE_ID) {
+            generate_metadata
+            generate_manifest
+            generate_spine
           }
+
+          @xml = nil
         }
 
         builder.doc
@@ -106,8 +66,91 @@ module Epuber
       end
 
 
-
       private
+
+      def generate_manifest
+        @xml.manifest {
+          @target.all_files.each { |file|
+            @xml.item(id: create_id_from_path(file.destination_path), href: file.destination_path, 'media-type' => mime_type_for(file))
+          }
+        }
+      end
+
+      def generate_spine
+        args = if @target.epub_version >= 3
+                 {}
+               else
+                 nav_file = @target.all_files.find { |file| file.destination_path.end_with?('.ncx') }
+                 raise 'not found nav file' if nav_file.nil?
+
+                 { toc: create_id_from_path(nav_file.destination_path) }
+               end
+
+        @xml.spine(args) {
+          visit_toc_items(@book.root_toc.child_items)
+        }
+      end
+
+      # @param toc_items [Array<Epuber::Book::TocItem>]
+      #
+      def visit_toc_items(toc_items)
+        toc_items.each { |child_item|
+          visit_toc_item(child_item)
+        }
+      end
+
+      # @param toc_item [Epuber::Book::TocItem]
+      #
+      def visit_toc_item(toc_item)
+        @xml.itemref(idref: toc_item.file_obj.destination_path)
+
+        visit_toc_items(toc_item.child_items)
+      end
+
+      def generate_metadata
+        epub_version = @target.epub_version
+
+        @xml.metadata {
+          if @book.title
+            if epub_version >= 3
+              @xml['dc'].title(@book.title, id: 'title')
+              @xml.meta('main', refines: '#title', property: 'title-type')
+            else
+              @xml['dc'].title(@book.title)
+            end
+          end
+
+          authors = @book.authors || []
+          authors.each_index { |idx|
+            author        = authors[idx]
+            author_id     = "author_#{idx + 1}"
+            author_id_ref = "##{author_id}"
+
+            if epub_version >= 3
+              @xml['dc'].creator(author.pretty_name, id: author_id)
+              @xml.meta(author.file_as, property: 'file-as', refines: author_id_ref)
+              @xml.meta(author.role, property: 'role', refines: author_id_ref, scheme: 'marc:relators')
+            else
+              @xml['dc'].creator(author.pretty_name)
+            end
+          }
+
+          @xml['dc'].publisher(@book.publisher) unless @book.publisher.nil?
+          @xml['dc'].language(@book.language) unless @book.language.nil?
+          @xml['dc'].date(@book.published.iso8601) unless @book.published.nil?
+
+          @xml.meta(name: 'cover', content: create_id_from_path(@target.cover_image.destination_path)) unless @target.cover_image.nil?
+
+          if epub_version >= 3
+            @xml.meta(Time.now.utc.iso8601, property: 'dcterms:modified')
+
+            if @target.is_ibooks
+              @xml.meta(@book.version, property: 'ibooks:version') unless @book.version.nil?
+              @xml.meta(@book.custom_fonts, property: 'ibooks:specified-fonts') unless @book.custom_fonts.nil?
+            end
+          end
+        }
+      end
 
       # Creates id from file path
       #
