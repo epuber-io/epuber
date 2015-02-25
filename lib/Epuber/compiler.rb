83555b20c2563c34a61db5eb5dfbd571cb855324
Roman Kříž <samnung@gmail.com>
2015-02-25 17:01:32 +0100

    [Compiler] refactoring, move file processing methods to separate file (compiler/file_processing.rb)

--- lib/Epuber/compiler.rb
+++ lib/Epuber/compiler.rb
@@ -27,6 +27,10 @@ module Epuber
     require_relative 'compiler/file'
     require_relative 'compiler/file_resolver'
 
+    require_relative 'compiler/file_processing'
+    include FileProcessing
+
+
     EPUB_CONTENT_FOLDER = 'OEBPS'
 
     GROUP_EXTENSIONS = {
@@ -68,9 +72,10 @@ module Epuber
 
       puts "  handling target #{@target.name.inspect} in build dir `#{build_folder}`"
 
-      process_toc_item(@target.root_toc)
+      parse_toc_item(@target.root_toc)
+      parse_target_file_requests
 
-      process_target_files
+      process_all_target_files
       generate_other_files
 
       # build folder cleanup
@@ -175,9 +180,9 @@ module Epuber
       process_file(opf_file)
 
       # generate mimetype file
-      mimetype_file                  = Epuber::Compiler::File.new(nil)
+      mimetype_file = Epuber::Compiler::File.new(nil)
       mimetype_file.package_destination_path = 'mimetype'
-      mimetype_file.content          = 'application/epub+zip'
+      mimetype_file.content                  = 'application/epub+zip'
       @file_resolver.add_file(mimetype_file, type: :package)
       process_file(mimetype_file)
 
@@ -190,7 +195,7 @@ module Epuber
       end
     end
 
-    def process_target_files
+    def parse_target_file_requests
       @target.files.each do |file_request|
         if file_request.only_one
           @file_resolver.add_file(File.new(file_request))
@@ -198,7 +203,9 @@ module Epuber
           @file_resolver.add_files(file_request)
         end
       end
+    end
 
+    def process_all_target_files
       @file_resolver.files_of(:manifest).each do |file|
         process_file(file)
       end
@@ -206,7 +213,7 @@ module Epuber
 
     # @param toc_item [Epuber::Book::TocItem]
     #
-    def process_toc_item(toc_item)
+    def parse_toc_item(toc_item)
       unless toc_item.file_request.nil?
         file_request = toc_item.file_request
 
@@ -217,165 +224,8 @@ module Epuber
 
       # process recursively other files
       toc_item.sub_items.each do |child|
-        process_toc_item(child)
+        parse_toc_item(child)
       end
-    end
-
-    # @param file [Epuber::Compiler::File]
-    #
-    def process_file(file)
-      dest_path = file.destination_path
-      FileUtils.mkdir_p(::File.dirname(dest_path))
-
-      if !file.content.nil?
-        from_source_and_old = !file.source_path.nil? && !FileUtils.uptodate?(dest_path, [file.source_path])
-        if from_source_and_old
-          # invalidate old content
-          file.content = nil
-
-          return process_file(file)
-        end
-
-        file_write(file)
-      elsif !file.source_path.nil?
-        file_source_path = file.source_path
-        file_extname = ::File.extname(file_source_path)
-
-        case file_extname
-        when *GROUP_EXTENSIONS[:text]
-          process_text_file(file)
-        when *GROUP_EXTENSIONS[:image]
-          process_image_file(file)
-        when *STATIC_EXTENSIONS
-          file_copy(file)
-        when '.styl'
-          file.content = Stylus.compile(::File.new(file_source_path))
-          file_write(file)
-        else
-          raise "unknown file extension #{file_extname} for file #{file.inspect}"
-        end
-      else
-        raise "don't know what to do with file #{file.inspect} at path #{file.source_path}"
-      end
-    end
-
-    # @param file [Epuber::Compiler::File]
-    #
-    def process_text_file(file)
-      source_path = file.source_path
-      source_extname = ::File.extname(source_path)
-
-      xhtml_content   = case source_extname
-                        when '.xhtml'
-                          ::File.read(source_path)
-                        when '.rxhtml'
-                          RubyTemplater.render_file(source_path)
-                        when '.bade'
-                          parsed = Bade::Parser.new(file: source_path).parse(::File.read(source_path))
-                          lam    = Bade::RubyGenerator.node_to_lambda(parsed, new_line: '\n', indent: '  ')
-                          lam.call
-                        else
-                          raise "Unknown text file extension #{source_extname}"
-                        end
-
-      xhtml_doc = Nokogiri::XML.parse(xhtml_content, nil, 'UTF-8')
-      # @type xhtml_doc [Nokogiri::XML::Document]
-
-      # add missing body element
-      if xhtml_doc.at_css('body').nil?
-        xhtml_doc.root.surround_with_element('body')
-      end
-
-      # add missing root html element
-      if xhtml_doc.at_css('html').nil?
-        attrs = {}
-        attrs['xmlns'] = 'http://www.w3.org/1999/xhtml'
-        attrs['xmlns:epub'] = 'http://www.idpf.org/2007/ops' if @target.epub_version >= 3
-        xhtml_doc.root.surround_with_element('html', attrs)
-      end
-
-      # add missing head in html
-      if xhtml_doc.at_css('html > head').nil?
-        html = xhtml_doc.css('html').first
-        head = xhtml_doc.create_element('head')
-        head << xhtml_doc.create_element('title', @book.title)
-
-        html.children.first.before(head)
-      end
-
-      head = xhtml_doc.at_css('html > head')
-      styles_hrefs = head.css('link[rel="stylesheet"]').map { |element| element.attribute('href').value }
-
-      paths_to_insert = @target.default_styles.map do |default_style_request|
-        @file_resolver.find_files_from_request(default_style_request) || []
-      end.flatten.map do |style_file|
-        @file_resolver.relative_path(file, style_file)
-      end.reject do |path|
-        styles_hrefs.include?(path)
-      end
-
-      paths_to_insert.each do |path_to_insert|
-        head << xhtml_doc.create_element('link', rel: 'stylesheet', href: path_to_insert, type: 'text/css')
-      end
-
-      # TODO: perform text transform
-      # TODO: perform analysis
-
-      file.content = xhtml_doc
-      file_write(file)
-    end
-
-    # @param file [Epuber::Compiler::File]
-    #
-    def process_image_file(file)
-      dest_path = file.destination_path
-      source_path = file.source_path
-
-      return if FileUtils.uptodate?(dest_path, [source_path])
-
-      img = Magick::Image::read(source_path).first
-
-      resolution = img.columns * img.rows
-      max_resolution = 2_000_000
-      if resolution > max_resolution
-        scale = max_resolution.to_f / resolution.to_f
-        puts "DEBUG: downscaling image #{source_path} with scale #{scale}"
-        img.scale!(scale)
-        img.write(dest_path)
-      else
-        file_copy(file)
-      end
-    end
-
-    # @param file [Epuber::Compiler::File]
-    #
-    def file_write(file)
-      dest_path = file.destination_path
-
-      original_content = if ::File.exists?(dest_path)
-                           ::File.read(dest_path)
-                         end
-
-      return if original_content == file.content || original_content == file.content.to_s
-
-      puts "DEBUG: writing to file #{dest_path}"
-
-      ::File.open(dest_path, 'w') do |file_handle|
-        file_handle.write(file.content)
-      end
-    end
-
-    # @param file [Epuber::Compiler::File]
-    #
-    def file_copy(file)
-      dest_path = file.destination_path
-      source_path = file.source_path
-
-      return if FileUtils.uptodate?(dest_path, [source_path])
-      return if ::File.exists?(dest_path) && FileUtils.compare_file(dest_path, source_path)
-
-      puts "DEBUG: copying file from #{source_path} to #{dest_path}"
-      FileUtils.cp(source_path, dest_path)
     end
 
     # @param cmd [String]
---
+++ lib/Epuber/compiler/file_processing.rb
@@ -0,0 +1,165 @@
+
+module Epuber
+  class Compiler
+    module FileProcessing
+      # @param file [Epuber::Compiler::File]
+      #
+      def process_file(file)
+        dest_path = file.destination_path
+        FileUtils.mkdir_p(::File.dirname(dest_path))
+
+        if !file.content.nil?
+          from_source_and_old = !file.source_path.nil? && !FileUtils.uptodate?(dest_path, [file.source_path])
+          if from_source_and_old
+            # invalidate old content
+            file.content = nil
+
+            return process_file(file)
+          end
+
+          file_write(file)
+        elsif !file.source_path.nil?
+          file_source_path = file.source_path
+          file_extname = ::File.extname(file_source_path)
+
+          case file_extname
+          when *GROUP_EXTENSIONS[:text]
+            process_text_file(file)
+          when *GROUP_EXTENSIONS[:image]
+            process_image_file(file)
+          when *STATIC_EXTENSIONS
+            file_copy(file)
+          when '.styl'
+            file.content = Stylus.compile(::File.new(file_source_path))
+            file_write(file)
+          else
+            raise "unknown file extension #{file_extname} for file #{file.inspect}"
+          end
+        else
+          raise "don't know what to do with file #{file.inspect} at path #{file.source_path}"
+        end
+      end
+
+      # @param file [Epuber::Compiler::File]
+      #
+      def process_image_file(file)
+        dest_path = file.destination_path
+        source_path = file.source_path
+
+        return if FileUtils.uptodate?(dest_path, [source_path])
+
+        img = Magick::Image::read(source_path).first
+
+        resolution = img.columns * img.rows
+        max_resolution = 2_000_000
+        if resolution > max_resolution
+          scale = max_resolution.to_f / resolution.to_f
+          puts "DEBUG: downscaling image #{source_path} with scale #{scale}"
+          img.scale!(scale)
+          img.write(dest_path)
+        else
+          file_copy(file)
+        end
+      end
+
+      # @param file [Epuber::Compiler::File]
+      #
+      def process_text_file(file)
+        source_path    = file.source_path
+        source_extname = ::File.extname(source_path)
+
+        xhtml_content   = case source_extname
+                          when '.xhtml'
+                            ::File.read(source_path)
+                          when '.rxhtml'
+                            RubyTemplater.render_file(source_path)
+                          when '.bade'
+                            parsed = Bade::Parser.new(file: source_path).parse(::File.read(source_path))
+                            lam    = Bade::RubyGenerator.node_to_lambda(parsed, new_line: '\n', indent: '  ')
+                            lam.call
+                          else
+                            raise "Unknown text file extension #{source_extname}"
+                          end
+
+        xhtml_doc = Nokogiri::XML.parse(xhtml_content, nil, 'UTF-8')
+        # @type xhtml_doc [Nokogiri::XML::Document]
+
+        # add missing body element
+        if xhtml_doc.at_css('body').nil?
+          xhtml_doc.root.surround_with_element('body')
+        end
+
+        # add missing root html element
+        if xhtml_doc.at_css('html').nil?
+          attrs = {}
+          attrs['xmlns'] = 'http://www.w3.org/1999/xhtml'
+          attrs['xmlns:epub'] = 'http://www.idpf.org/2007/ops' if @target.epub_version >= 3
+          xhtml_doc.root.surround_with_element('html', attrs)
+        end
+
+        # add missing head in html
+        if xhtml_doc.at_css('html > head').nil?
+          html = xhtml_doc.css('html').first
+          head = xhtml_doc.create_element('head')
+          head << xhtml_doc.create_element('title', @book.title)
+
+          html.children.first.before(head)
+        end
+
+        head = xhtml_doc.at_css('html > head')
+        styles_hrefs = head.css('link[rel="stylesheet"]').map { |element| element.attribute('href').value }
+
+        paths_to_insert = @target.default_styles.map do |default_style_request|
+          @file_resolver.find_files_from_request(default_style_request) || []
+        end.flatten.map do |style_file|
+          @file_resolver.relative_path(file, style_file)
+        end.reject do |path|
+          styles_hrefs.include?(path)
+        end
+
+        paths_to_insert.each do |path_to_insert|
+          head << xhtml_doc.create_element('link', rel: 'stylesheet', href: path_to_insert, type: 'text/css')
+        end
+
+        # TODO: perform text transform
+        # TODO: perform analysis
+
+        file.content = xhtml_doc
+        file_write(file)
+      end
+
+
+      # @param file [Epuber::Compiler::File]
+      #
+      def file_write(file)
+        dest_path = file.destination_path
+
+        original_content = if ::File.exists?(dest_path)
+                             ::File.read(dest_path)
+                           end
+
+        return if original_content == file.content || original_content == file.content.to_s
+
+        puts "DEBUG: writing to file #{dest_path}"
+
+        ::File.open(dest_path, 'w') do |file_handle|
+          file_handle.write(file.content)
+        end
+      end
+
+      # @param file [Epuber::Compiler::File]
+      #
+      def file_copy(file)
+        dest_path = file.destination_path
+        source_path = file.source_path
+
+        return if FileUtils.uptodate?(dest_path, [source_path])
+        return if ::File.exists?(dest_path) && FileUtils.compare_file(dest_path, source_path)
+
+        puts "DEBUG: copying file from #{source_path} to #{dest_path}"
+        FileUtils.cp(source_path, dest_path)
+      end
+
+    end
+  end
+end
