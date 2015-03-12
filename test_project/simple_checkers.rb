38fd5103479819907d4ef7c2c1c1799c340a6ce2
Roman Kříž <samnung@gmail.com>
2015-03-13 00:33:28 +0100

    [test_project] add standar checkers from epubtool

--- test_project/simple_checkers.rb
+++ test_project/simple_checkers.rb
@@ -1,8 +1,11 @@
+# encoding utf-8
 
-check :result_text_xhtml_string, :always do |checker, text|
-  checker.should_not_find(/Jason Fried/, 'Found Jason Fried, kill him!')
-end
+check :result_text_xhtml_string, :before_release do |checker|
+  checker.should_not_contain(/(̌|́)/i, 'Contains strange diacritic characters')
 
-check :result_text_xhtml_string, :before_release do |checker, text|
-  puts ':release'
+  checker.should_not_contain(/[-–—][-–—]+/i, 'Contains redundant dashes')
+
+  checker.should_not_contain(/( | )(,|\.|;|:)( | )/i, 'Contains alone `(,|.|;|:)`')
+
+  checker.should_not_contain(/ -( |,|\.)/i, 'Contains alone ` -( |,|.)`')
 end
