4f399afe1fc10753582acaba831639b2d0f15457
Roman Kříž <samnung@gmail.com>
2015-10-05 17:16:50 +0200

    [epuber.rb] remove require of bundler/setup

--- lib/epuber.rb
+++ lib/epuber.rb
@@ -3,8 +3,6 @@
 module Epuber
   require_relative 'epuber/version'
 
-  require 'bundler/setup'
-
   autoload :Book, 'epuber/book'
   autoload :Command, 'epuber/command'
   autoload :Compiler, 'epuber/compiler'
